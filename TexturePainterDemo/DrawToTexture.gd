f3d2a82064379f296309aecaf4452d19b027df99
Tapio Pyrhönen <tapio.pyrhonen@gmail.com>
2017-02-09 10:22:01 +0200

    Skip backfaces when drawing
    Use the dot product of the face normal and cameras forward vector to check if the face is facing the camera.

--- TexturePainterDemo/DrawToTexture.gd
+++ TexturePainterDemo/DrawToTexture.gd
@@ -35,48 +35,56 @@ func shoot(var color):
 		var i=0
 		while(i<mdt.get_face_count()):
 			
-			# indices of the verts
-			var i0 = mdt.get_face_vertex(i,0)
-			var i1 = mdt.get_face_vertex(i,1)
-			var i2 = mdt.get_face_vertex(i,2)
+			# skip backfaces
+			var n = mdt.get_face_normal(i)
+			n = object.get_transform().xform(n)
 			
-			# get unprojected vertices (3d->2d)
-			var A = camera.unproject_position(object.get_transform().xform(mdt.get_vertex(i0)))
-			var B = camera.unproject_position(object.get_transform().xform(mdt.get_vertex(i1)))
-			var C = camera.unproject_position(object.get_transform().xform(mdt.get_vertex(i2)))
-			var P = mousepos
+			var d = n.dot(camera.get_transform().basis.z)
 			
-			# Next comes some weird black magic that I don't really understand
-			# http://blackpawn.com/texts/pointinpoly/
-			
-			# Compute vectors        
-			var v0 = C - A
-			var v1 = B - A
-			var v2 = P - A
-			
-			# Compute dot products
-			var dot00 = v0.dot(v0)
-			var dot01 = v0.dot(v1)
-			var dot02 = v0.dot(v2)
-			var dot11 = v1.dot(v1)
-			var dot12 = v1.dot(v2)
-			
-			# Compute barycentric coordinates
-			var invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
-			var u = (dot11 * dot02 - dot01 * dot12) * invDenom
-			var v = (dot00 * dot12 - dot01 * dot02) * invDenom
-			
-			# Check if point is in triangle
-			if((u >= 0) && (v >= 0) && (u + v < 1)):
+			if(d>0):
 				
-				# perform the same weird math for the UV but inverted
-				var a = mdt.get_vertex_uv(i0)
-				var b = mdt.get_vertex_uv(i1)
-				var c = mdt.get_vertex_uv(i2)
-				var P = a + u * (c - a) + v * (b - a)
+				# indices of the verts
+				var i0 = mdt.get_face_vertex(i,0)
+				var i1 = mdt.get_face_vertex(i,1)
+				var i2 = mdt.get_face_vertex(i,2)
 				
-				# finally draw the pixel
-				meshinst.plot_px(P.x,P.y,color)
+				# get unprojected vertices (3d->2d)
+				var A = camera.unproject_position(object.get_transform().xform(mdt.get_vertex(i0)))
+				var B = camera.unproject_position(object.get_transform().xform(mdt.get_vertex(i1)))
+				var C = camera.unproject_position(object.get_transform().xform(mdt.get_vertex(i2)))
+				var P = mousepos
+				
+				# Next comes some weird black magic that I don't really understand
+				# http://blackpawn.com/texts/pointinpoly/
+				
+				# Compute vectors        
+				var v0 = C - A
+				var v1 = B - A
+				var v2 = P - A
+				
+				# Compute dot products
+				var dot00 = v0.dot(v0)
+				var dot01 = v0.dot(v1)
+				var dot02 = v0.dot(v2)
+				var dot11 = v1.dot(v1)
+				var dot12 = v1.dot(v2)
+				
+				# Compute barycentric coordinates
+				var invDenom = 1 / (dot00 * dot11 - dot01 * dot01)
+				var u = (dot11 * dot02 - dot01 * dot12) * invDenom
+				var v = (dot00 * dot12 - dot01 * dot02) * invDenom
+				
+				# Check if point is in triangle
+				if((u >= 0) && (v >= 0) && (u + v < 1)):
+					
+					# perform the same weird math for the UV but inverted
+					var a = mdt.get_vertex_uv(i0)
+					var b = mdt.get_vertex_uv(i1)
+					var c = mdt.get_vertex_uv(i2)
+					var P = a + u * (c - a) + v * (b - a)
+					
+					# finally draw the pixel
+					meshinst.plot_px(P.x,P.y,color)
 				
 			i+=1
 		
