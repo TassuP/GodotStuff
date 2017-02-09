28b237e127670b62b8b123e47a799824017dbdc5
Tapio Pyrhönen <tapio.pyrhonen@gmail.com>
2017-02-09 14:19:17 +0200

    Added slider to change brush size

--- TexturePainterDemo/TextureUpdater.gd
+++ TexturePainterDemo/TextureUpdater.gd
@@ -2,6 +2,8 @@ extends MeshInstance
 
 export(String, FILE, "*.png") var texture_path
 
+var brushsize = 3.0
+
 var imgtex
 var img
 var sz
@@ -20,8 +22,14 @@ func plot_px(x,y, c):
 	
 	x=clamp(x*sz.x,0,sz.x-1)
 	y=clamp(y*sz.y,0,sz.y-1)
-#	print(x," ",y)
-	img.put_pixel(x,y,c)
+
+	for xx in range(x-brushsize, x+brushsize+1):
+		for yy in range(y-brushsize, y+brushsize+1):
+			if(xx >= 0 && xx < sz.x):
+				if(yy >= 0 && yy < sz.y):
+					if(Vector2(xx,yy).distance_to(Vector2(x,y)) <= brushsize):
+						img.put_pixel(xx,yy,c)
+	
 	imgtex.set_data(img)
 	get_material_override().set_texture(FixedMaterial.PARAM_DIFFUSE,imgtex)
 	
@@ -34,3 +42,6 @@ func _on_ClearButton_button_down():
 			img.put_pixel(x,y,col)
 	imgtex.set_data(img)
 	get_material_override().set_texture(FixedMaterial.PARAM_DIFFUSE,imgtex)
+
+func _on_change_brushsize( value ):
+	brushsize = value
